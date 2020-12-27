1d3b6492f4367104fb980a6d902f3e3efd6b8426
Hexirp <Hexirp@users.noreply.github.com>
2020-12-27 15:18:56 +0900

    Update: 実際のコードを書く
    とっかかりとして必要なため。

---
+++ citation.lua
@@ -0,0 +1,24 @@
+local function cite_fact(frame)
+  local username = frame[1]
+
+  return $"${user(username)}: [[広く知られた事実]]である。"
+end
+
+local function cite_obvious(frame)
+  local username = frame[1]
+
+  return $"${user(username)}: [[自明な事柄]]である。"
+end
+
+local function main(frame)
+  local subcommand = {
+    fact = cite_fact,
+    obvious = cite_obvious,
+  }
+
+  local cite_type = frame["type"]
+
+  return subcommand[cite_type](frame)
+end
+
+return {main = main}
